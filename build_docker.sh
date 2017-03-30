22528159dcea82a10f5e9b33efb14282f0ae7190
Charles Reid <charlesreid1@gmail.com>
2017-03-30 23:00:58 +0000

    updating docker script to also check for configuration file

--- build_docker.sh
+++ build_docker.sh
@@ -6,10 +6,14 @@
 
 export CERT="fullchain.pem"
 export KEY="privkey.pem"
+export CONF="stunnel.conf"
 
-if [ -f $CERT -a -f $KEY ]; then
-	cp stunnel.server.ssh_over_443.conf stunnel.conf
+if [ -f $CERT -a -f $KEY -a $CONF ]; then
 	docker build -t cmr_stunnel .
 else
-	echo "Missing fullchain.pem and privkey.pem - check /etc/letsencrypt/live/domain.com/"
+	echo "Missing one of the following:"
+	echo "stunnel.conf, fullchain.pem, privkey.pem"
+	echo ""
+	echo "Copy an examkple stunel configuration file, or use your own."
+	echo "Check /etc/letsencrypt/live/domain.com/ or /etc/ssl/ for certificates."
 fi
