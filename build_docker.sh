97a2acd928fff93a5fe6e953efa465029e482690
Charles Reid <charlesreid1@gmail.com>
2017-03-30 22:57:53 +0000

    adding check to ensure private key/certificate files exist

--- build_docker.sh
+++ build_docker.sh
@@ -4,4 +4,12 @@
 # 
 # https://charlesreid1.com/wiki/Docker/Dockerfiles
 
-docker build -t cmr_stunnel .
+export CERT="fullchain.pem"
+export KEY="privkey.pem"
+
+if [ -f $CERT -a -f $KEY ]; then
+	cp stunnel.server.ssh_over_443.conf stunnel.conf
+	docker build -t cmr_stunnel .
+else
+	echo "Missing fullchain.pem and privkey.pem - check /etc/letsencrypt/live/domain.com/"
+fi
